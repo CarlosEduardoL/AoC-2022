e4039e8a0818ed8cca153563f295402bc7df7146
carlos lizalda <carlos.lizalda1@gmail.com>
2022-12-04 11:53:50 -0500

    day4

---
+++ src/day4.rs
@@ -0,0 +1,67 @@
+use std::result;
+
+struct Range {
+    min: i32,
+    max: i32,
+}
+
+impl Range {
+    fn contains(&self, other: &Range) -> bool {
+        self.min <= other.min && self.max >= other.max
+    }
+    fn intersects(&self, other: &Range) -> bool {
+        !(self.max < other.min || self.min > other.max) 
+    } 
+}
+
+pub fn p1() {
+    let input = include_str!("inputs\\day4.txt").trim_end();
+    let result = input
+        .lines()
+        .map(|l| {
+            let mut ranges = l.split(",").map(|p| {
+                let mut ids = p.split("-").map(|id| id.parse::<i32>().unwrap());
+                Range {
+                    min: ids.next().unwrap(),
+                    max: ids.next().unwrap(),
+                }
+            });
+            (ranges.next().unwrap(), ranges.next().unwrap())
+        })
+        .map(|(r1, r2)| {
+            (r1.contains(&r2) || r2.contains(&r1)) as i32
+        })
+        .sum::<i32>();
+    println!("{result}");
+}
+
+#[test]
+fn part1() {
+    //441
+    p1()
+}
+
+pub fn p2() {
+    let input = include_str!("inputs\\day4.txt").trim_end();
+    let result = input
+        .lines()
+        .map(|l| {
+            let mut ranges = l.split(",").map(|p| {
+                let mut ids = p.split("-").map(|id| id.parse::<i32>().unwrap());
+                Range {
+                    min: ids.next().unwrap(),
+                    max: ids.next().unwrap(),
+                }
+            });
+            (ranges.next().unwrap(), ranges.next().unwrap())
+        })
+        .map(|(r1, r2)| r1.intersects(&r2) as i32)
+        .sum::<i32>();
+    println!("{result}");
+}
+
+#[test]
+fn part2() {
+    //861
+    p2()
+}
